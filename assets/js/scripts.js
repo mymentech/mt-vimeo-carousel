756343cba7fa62b9ee4ce2d919c63924a9ea85f5
MymenTech <mymentech@gmail.com>
2018-12-14 17:43:43 +0600

    Added responsive feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,36 @@
-;(function($){
-	$(document).ready(function(){
-		$('.make-carousel .vc_column-inner .wpb_wrapper').slick({
+;(function ($) {
+    $(document).ready(function () {
+        $('.make-carousel .vc_column-inner .wpb_wrapper').slick({
 
-		  infinite: true,
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
 
-		  responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 1,
-		        infinite: true,
-		        dots: false
-		      }
-		    },
-		    {
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-		    ]
-		  
-		});
-	
+        });
+    });
 })(jQuery);
